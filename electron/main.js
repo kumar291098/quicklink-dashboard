c411195a691fdfc13db5c0d1d0bcf5f2332c6389
Avnish <kumara291098@gmail.com>
2026-05-23 15:09:42 +0530

    Add Electron setup and app icon updates

---
+++ electron/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    title: 'QuickLink Dashboard',
+    icon: path.join(__dirname, 'quicklink-icon.png'),
+    width: 1400,
+    height: 900,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  })
+
+  win.loadURL('http://localhost:5173')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
